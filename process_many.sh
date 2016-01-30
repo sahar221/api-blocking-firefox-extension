4e188394e7a8bb7d7ec2a1be65e085d6c7c88016
bits <bits@10-7-44-183.uic.edu>
2016-01-30 16:12:28 -0600

    getting the 'forever' version to work

--- process_many.sh
+++ process_many.sh
@@ -4,10 +4,10 @@ if [[ $# < 2 ]]; then
 	echo "Usage: ./process_many.sh <domain list> <path> [should_block]";
 	exit 1;
 fi;
-SOURCE_FILE=$0;
-DEST_DIR=$1;
+SOURCE_FILE=$1;
+DEST_DIR=$2;
 
-if [[ -z $2 ]]; then
+if [[ -z $3 ]]; then
 	BLOCK_FLAG="";
 else
 	BLOCK_FLAG="-e";
@@ -15,15 +15,15 @@ fi;
 
 while [[ 1 ]]; do
 
-	ALL_LINES=`shuf $SOURCE_FILE`;
+	ALL_LINES=`cat $SOURCE_FILE | shuf`;
 
 	for LINE in $ALL_LINES; do
 
-		DOMAIN=`echo $LINE | awk '{print $1}'`;
-		SUBDOMAINS=`echo $LINE | awk '{print $2}'`;
+		DOMAIN=`echo $LINE | awk -F'-' '{print $1}'`;
+		SUBDOMAINS=`echo $LINE | awk -F'-' '{print $2}'`;
 		INDEX=0;
 
-		while [[ ! -f $DEST_DIR/$DOMAIN-$INDEX.json ]]; do
+		while [[ -e $DEST_DIR/$DOMAIN-$INDEX.json ]]; do
 			INDEX=$(($INDEX + 1));
 		done;
 
@@ -32,7 +32,9 @@ while [[ 1 ]]; do
 		else
 			SUBDOMAINS_ARG="";
 		fi;
+
 		JSON=`./run.sh -b /home/bits/Code/firefox-43.0.4/obj-x86_64-unknown-linux-gnu/dist/bin/firefox $BLOCK_FLAG -u http://$DOMAIN $SUBDOMAINS_ARG`;
+		echo "Writing $DEST_DIR/$DOMAIN-$INDEX.json";
 		echo $JSON > $DEST_DIR/$DOMAIN-$INDEX.json;
 	done;
 done;
