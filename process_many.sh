d39e1240eaa3c74896a96c45fd763efe8a9e5ff5
bits <bits@10-7-44-183.uic.edu>
2016-01-30 15:39:19 -0600

    updating processing script to run forever

--- process_many.sh
+++ process_many.sh
@@ -1,14 +1,38 @@
 #!/usr/bin/env bash
 
-while read LINE; do
-	DOMAIN=`echo $LINE | awk '{print $1}'`;
-	SUBDOMAINS=`echo $LINE | awk '{print $2}'`;
-	if [[ -n $SUBDOMAINS ]]; then
-		SUBDOMAINS_ARG="-d $SUBDOMAINS";
-	else
-		SUBDOMAINS_ARG="";
-	fi;
-	JSON=`./run.sh -b /home/bits/Code/firefox-43.0.4/obj-x86_64-unknown-linux-gnu/dist/bin/firefox -e -u http://$DOMAIN $SUBDOMAINS_ARG`;
-	echo $JSON > ~/Desktop/data/$DOMAIN.json;
-done;
+if [[ $# < 2 ]]; then
+	echo "Usage: ./process_many.sh <domain list> <path> [should_block]";
+	exit 1;
+fi;
+SOURCE_FILE=$0;
+DEST_DIR=$1;
 
+if [[ -z $2 ]]; then
+	BLOCK_FLAG="";
+else
+	BLOCK_FLAG="-e";
+fi;
+
+while [[ 1 ]]; do
+
+	ALL_LINES=`shuf $SOURCE_FILE`;
+
+	for LINE in $ALL_LINES; do
+
+		DOMAIN=`echo $LINE | awk '{print $1}'`;
+		SUBDOMAINS=`echo $LINE | awk '{print $2}'`;
+		INDEX=0;
+
+		while [[ ! -f $DEST_DIR/$DOMAIN-$INDEX.json ]]; do
+			INDEX=$(($INDEX + 1));
+		done;
+
+		if [[ -n $SUBDOMAINS ]]; then
+			SUBDOMAINS_ARG="-d $SUBDOMAINS";
+		else
+			SUBDOMAINS_ARG="";
+		fi;
+		JSON=`./run.sh -b /home/bits/Code/firefox-43.0.4/obj-x86_64-unknown-linux-gnu/dist/bin/firefox $BLOCK_FLAG -u http://$DOMAIN $SUBDOMAINS_ARG`;
+		echo $JSON > $DEST_DIR/$DOMAIN-$INDEX.json;
+	done;
+done;
